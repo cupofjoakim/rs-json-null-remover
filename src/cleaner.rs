028caaa0e0ff5dd17ea0b647c4ac741ed0a5f4dc
Joakim Wimmerstedt <joakim.wimmerstedt@gmail.com>
2024-10-24 14:01:20 +0200

    TIL .retain exists

--- src/cleaner.rs
+++ src/cleaner.rs
@@ -10,22 +10,7 @@ pub fn remove_null_values(json_data: &mut Value) -> Result<(), io::Error> {
 }
 
 fn remove_null_values_from_object(object: &mut Map<String, Value>) -> Result<(), io::Error> {
-    // Collect keys to remove
-    let keys_to_remove: Vec<String> = object
-        .iter()
-        .filter_map(|(key, value)| {
-            if value.is_null() {
-                Some(key.clone())
-            } else {
-                None
-            }
-        })
-        .collect();
-
-    for key in keys_to_remove {
-        log::debug!("Removing key {}", key);
-        object.remove(&key);
-    }
+    object.retain(|_key, value| !value.is_null());
 
     // Go a lever deeper w recursion
     for value in object.values_mut() {
@@ -36,17 +21,7 @@ fn remove_null_values_from_object(object: &mut Map<String, Value>) -> Result<(),
 }
 
 fn remove_null_values_from_array(array: &mut Vec<Value>) -> Result<(), io::Error> {
-    // Collect indices to remove
-    let indices_to_remove: Vec<usize> = array
-        .iter()
-        .enumerate()
-        .filter_map(|(index, value)| if value.is_null() { Some(index) } else { None })
-        .collect();
-
-    for index in indices_to_remove.iter().rev() {
-        log::debug!("Removing item with index {}", index);
-        array.remove(*index);
-    }
+    array.retain(|value| !value.is_null());
 
     // Go a lever deeper w recursion
     for value in array.iter_mut() {
